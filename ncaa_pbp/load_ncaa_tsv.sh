811cad24271d8b8714fe8446a4a934050efddb07
Christopher D. Long <galizur@gmail.com>
2015-09-22 14:21:25 -0400

    Load all TSV data; note there are some issues with the team schedule files that will need to be addressed.

--- ncaa_pbp/load_ncaa_tsv.sh
+++ ncaa_pbp/load_ncaa_tsv.sh
@@ -11,30 +11,34 @@ fi
 
 psql basketball-w -f loaders_tsv/create_ncaa_pbp_schema.sql
 
-cp tsv/ncaa_teams_2015_1.tsv /tmp/ncaa_teams.tsv
+tail -q -n+2 tsv/ncaa_teams_*.tsv >> /tmp/ncaa_teams.tsv
 psql basketball-w -f loaders_tsv/load_ncaa_teams.sql
 rm /tmp/ncaa_teams.tsv
 
-cp tsv/ncaa_team_schedules_mt_2015_1.tsv /tmp/ncaa_team_schedules.tsv
+tail -q -n+2 tsv/ncaa_team_schedules_mt_2015_1.tsv >> /tmp/ncaa_team_schedules.tsv
 psql basketball-w -f loaders_tsv/load_ncaa_team_schedules.sql
 rm /tmp/ncaa_team_schedules.tsv
 
-cp tsv/ncaa_games_box_scores_mt_2015_1.tsv.gz /tmp/ncaa_games_box_scores.tsv.gz
-gzip -d /tmp/ncaa_games_box_scores.tsv.gz
+cp tsv/ncaa_games_box_scores_mt_*.tsv.gz /tmp
+pigz -d /tmp/ncaa_games_box_scores_mt_*.tsv.gz
+tail -q -n+2 /tmp/ncaa_games_box_scores_mt_*.tsv >> /tmp/ncaa_games_box_scores.tsv
 psql basketball-w -f loaders_tsv/load_ncaa_box_scores.sql
-rm /tmp/ncaa_games_box_scores.tsv
+rm /tmp/ncaa_games_box_scores*.tsv
 
-cp tsv/ncaa_team_rosters_mt_2015_1.tsv /tmp/ncaa_team_rosters.tsv
+tail -q -n+2 tsv/ncaa_team_rosters_mt_*.tsv >> /tmp/ncaa_team_rosters.tsv
 psql basketball-w -f loaders_tsv/load_ncaa_team_rosters.sql
 rm /tmp/ncaa_team_rosters.tsv
 
-cp tsv/ncaa_games_periods_mt_2015_1.tsv /tmp/ncaa_games_periods.tsv
+tail -q -n+2 tsv/ncaa_games_periods_mt_*.tsv >> /tmp/ncaa_games_periods.tsv
 rpl "[" "{" /tmp/ncaa_games_periods.tsv
 rpl "]" "}" /tmp/ncaa_games_periods.tsv
 psql basketball-w -f loaders_tsv/load_ncaa_games_periods.sql
 rm /tmp/ncaa_games_periods.tsv
 
-cp tsv/ncaa_games_play_by_play_mt_2015_1.tsv.gz /tmp/ncaa_games_play_by_play.tsv.gz
-gzip -d /tmp/ncaa_games_play_by_play.tsv.gz
+cp tsv/ncaa_games_play_by_play_mt_*.tsv.gz /tmp
+pigz -d /tmp/ncaa_games_play_by_play_mt_*.tsv.gz
+tail -q -n+2 /tmp/ncaa_games_play_by_play_mt_*.tsv >> /tmp/ncaa_games_play_by_play.tsv
 psql basketball-w -f loaders_tsv/load_ncaa_games_play_by_play.sql
-rm /tmp/ncaa_games_play_by_play.tsv
+rm /tmp/ncaa_games_play_by_play*.tsv
+
+exit
